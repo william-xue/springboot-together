5d6a4e47dd92930aa04290229e73cd5604667988
xueyuan <xue.yuan.99999@gmail.com>
2022-11-01 17:57:22 +0800

    init

---
+++ src/main/java/com/itheima/reggie/service/DishService.java
@@ -0,0 +1,14 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Dish;
+
+public interface DishService extends IService<Dish> {
+    void saveWithFlavor(DishDto dishDto);
+
+    //根据id查询菜品信息和对应的口味信息
+    public DishDto getByIdWithFlavor(Long id);
+    //更新菜品信息，同时更新对应的口味信息
+    public void updateWithFlavor(DishDto dishDto);
+}
---
+++ src/main/java/com/itheima/reggie/service/SetmealService.java
@@ -0,0 +1,16 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Setmeal;
+
+import java.util.List;
+
+public interface SetmealService extends IService<Setmeal> {
+    public void saveWithDish(SetmealDto setmealDto);
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     * @param ids
+     */
+    public void removeWithDish(List<Long> ids);
+}
